04550662d9cbeac9cbc5edacf0b86ecc6e4c06b4
ashokk <ashokk@nextgen.com>
2022-12-11 19:51:35 +0530

    this branch has CRUD operation with local storage

--- new_payroll-form.js
+++ new_payroll-form.js
@@ -17,18 +17,56 @@ window. addEventListener( 'DOMContentLoaded', (event) => {
     
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salary.addEventListener('input', function() {
-        output.textContent = salary.value;
-    });
+
+    //not required------------------------------------------------------------------
+    let update = localStorage.getItem("update");
+    localStorage.setItem("update","");
+    if(update != "" || update != null){
+        let arr = JSON.parse(localStorage.getItem("EmployeeList"));
+        let index = arr.findIndex(x=> x._name == update);
+        let employee = arr[index];
+        if(employee != null){
+            // set them to elements
+            name.value = employee._name;
+            //for profic pic
+            let profiles = document.querySelectorAll('[name=profile]');
+            let profilesArr = Array.from(profiles);
+            let pIndex = profilesArr.findIndex(x=> x.value == employee._profilePic);
+            profilesArr[pIndex].checked = true;
+
+            //gender
+            let genders = document.querySelectorAll('[name=gender]');
+            let genderArr = Array.from(genders);
+            let gIndex = genderArr.findIndex(x=>x.value == employee._gender);
+            genderArr[gIndex].checked=true;
+
+            // department
+            let departments = document.querySelectorAll('[name=department]');
+            let departmentsArr = Array.from(departments);
+            let filteredDepartments = departmentsArr.filter(x=> employee._department.includes(x.value));
+            filteredDepartments.forEach(x=>x.checked=true);
+
+            // salary
+            let salaryElement = document.querySelector('[name=salary]');
+            let salaryLablel = document.querySelector('#salary-lbl');
+            salaryElement.value=employee._salary;
+            salaryLablel.value=employee._salary;
+
+            // notes
+            let notesElement = document.querySelector('[name=Notes]');
+            notesElement.value=employee._note;
+        }
+    }
 });
 
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayroll();
+        window.location = 'home.html';
     } catch (e) {
         return;
     }
+    
 }
 
 const createEmployeePayroll=()=>{
@@ -47,7 +85,7 @@ employeePayrollData.note = getInputValueById( '#notes');
 let date = getInputValueById( '#day')+" "+getInputValueById('#month')+" "+
     getInputValueById('#year');
 employeePayrollData.startDate = new Date(date);
-alert (employeePayrollData.toString());
+updateLocalStorage(employeePayrollData);
 return employeePayrollData;
 }
 
@@ -72,3 +110,26 @@ const getInputElementValue = (id) => {
     return value;
 }
 
+let updateLocalStorage = (employee) => {
+let employeeArr = null;
+employeeArr = JSON.parse(localStorage.getItem("EmployeeList"));
+if(employeeArr == null){
+    employeeArr = [employee];
+}
+else{
+    let index = employeeArr.findIndex(x=> x._name == employee._name);
+    if(index<0){
+        employeeArr.push(employee);
+    }
+    else{
+        employeeArr.splice(index,0,employee);
+    }
+}
+localStorage.setItem("EmployeeList", JSON.stringify(employeeArr));
+let st = "";
+employeeArr = JSON.parse(localStorage.getItem("EmployeeList"));
+employeeArr.forEach(x=>{
+st+=x.toString()+"\n-----------------\n"
+});
+// alert(st);
+};
